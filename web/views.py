d7c7d9cbd10e55d8e67e801165ac7d534870db5f
ZuiYee <39691460+ZuiYee@users.noreply.github.com>
2019-01-12 17:16:47 +0800

    图片展示
    将图片传到前端展示

--- web/views.py
+++ web/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import IndexPicture, Project
 
 def index(request):
-    return render(request, 'init/index.html')
+    indexAllPictures = IndexPicture.objects.all()
+    context = {}
+    Last = indexAllPictures.last()
+    secondLasts = indexAllPictures.filter(pk=indexAllPictures.last().pk - 1)
+    if secondLasts:
+        secondLast = secondLasts[0]
+    thirdLasts = indexAllPictures.filter(pk=indexAllPictures.last().pk - 1)
+    if thirdLasts:
+        thirdLast = thirdLasts[0]
+    context['Last'] = Last
+    context['secondLasts'] = secondLast
+    context['thirdLasts'] = thirdLast
+    return render(request, 'init/index.html', context)
 
 def services(request):
-    return render(request, 'init/services.html')
+    allProject = Project.objects.all()
+    context = {}
+    context['allProject'] = allProject
+    return render(request, 'init/services.html', context)
 
 
 def test1(request):
