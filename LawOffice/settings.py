7bf057354d834639d1ef0b9de4252bcc5d1a7fc4
seekun <tmsshikun@gmail.com>
2019-01-13 21:45:43 +0800

    change setting file for deploy

--- LawOffice/settings.py
+++ LawOffice/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', 'ahutong.com', '193.112.97.157']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.ahutong.com', '193.112.97.157']
 
 # AUTH_USER_MODEL = 'users.User'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
