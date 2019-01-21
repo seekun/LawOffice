b935b1ea355c26b02a5a01eb30a71e9890fb32ea
seekun <tmsshikun@gmail.com>
2019-01-21 22:28:06 +0800

    fix template error

--- fabfile.py
+++ fabfile.py
@@ -5,13 +5,13 @@ GIT_REPO = "https://github.com/seekun/LawOffice.git"
 # GIT_REPO = "https://gitee.com/seekun/ahu.git"
 
 env.user = 'ubuntu'
-env.password = '7bRnLy^$^!sYSRMK'
-env.hosts = ['www.tmsshikun.xyz']
+env.password = 'sk13008587515'
+env.hosts = ['www.seekun.cn']
 # env.hosts = ['193.112.97.157']
 env.port = '22'
 
 def deploy():
-    source_folder = '/home/ubuntu/sites/demo.tmsshikun.xyz/LawOffice'
+    source_folder = '/home/ubuntu/sites/demo.seekun.cn/LawOffice'
 
     run('cd %s && git pull' % source_folder )
     run("""
@@ -20,12 +20,12 @@ def deploy():
         ../env/bin/python manage.py makemigrations&&
         ../env/bin/python manage.py migrate --run-syncdb
         """.format(source_folder))
-    sudo('restart gunicorn-demo.tmsshikun.xyz')
+    sudo('restart gunicorn-demo.seekun.cn')
     sudo('service nginx reload')
 
 
 def restart():
-    sudo('restart gunicorn-demo.tmsshikun.xyz')
+    sudo('restart gunicorn-demo.seekun.cn')
     sudo('service nginx reload')
 
 # python3 users/compressImage.py
